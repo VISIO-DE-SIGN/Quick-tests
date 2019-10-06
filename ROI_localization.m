4a80e4c1d12e16ff068f8474cdab499314b687e0
VictorCadix <31282394+VictorCadix@users.noreply.github.com>
2019-10-06 21:30:28 +0200

    getBlobImage function added

--- ROI_localization.m
+++ ROI_localization.m
@@ -1,40 +1,22 @@
 dataset_path = getenv('Dataset_path');
 image = strcat(dataset_path, "\camera00\00\image.000058.jp2");
 I = imread(image);
-imshow(I)
+%imshow(I)
 
 [c,r,dim] = size(I)
 pps_h = c/20;       %pixels per square horizontal
 res_h = c/pps_h
 res_v = round(r/pps_h)
 Ismall = imresize(I,[res_h,res_v]);
-figure
-imshow(Ismall,'InitialMagnification',2000)
+%figure
+%imshow(Ismall,'InitialMagnification',2000)
 
 % para el canal azul
-figure
-imshow(Ismall(:,:,3),'InitialMagnification',2000) 
+%figure
+%imshow(Ismall(:,:,3),'InitialMagnification',2000) 
 Ismall_B = Ismall(:,:,3);
 
-diff = zeros(res_h,26);
-for i = 1:res_h
-    for j = 1:res_v
-        cum = 0;
-        for m = i-1:i+1
-            if m-1 < 1 || m+1 > res_h
-                continue
-            end
-            for n = j-1:j+1
-                if n-1 < 1 || n+1 > res_v
-                    continue
-                end
-                dif = abs(double(Ismall_B(i,j)) - double(Ismall_B(m,n)));
-                cum = cum + dif;
-            end
-        end
-        diff(i,j) = cum;
-    end
-end
+diff = getBlobImage(Ismall_B);
 
 %normalizamos la imagen
 diff = diff / max(diff,[],'all');
